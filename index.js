da577701f924198445b89a96c91c3175c8f4b387
StarkElite <circuit369dg@hotmail.com>
2026-05-11 19:21:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ const { Client, LocalAuth } = require("whatsapp-web.js");
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    webVersionCache: {
-        type: "remote",
-        remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html"
-    },
     puppeteer: {
         headless: true,
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
         args: [
             "--no-sandbox",
             "--disable-setuid-sandbox",
