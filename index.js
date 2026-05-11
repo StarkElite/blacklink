00430707ff0b92fbb365771ed2196a83b866d01c
StarkElite <circuit369dg@hotmail.com>
2026-05-11 18:08:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const qrcode = require("qrcode-terminal");
 const { Client, LocalAuth } = require("whatsapp-web.js");
 
 const client = new Client({
-authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+
+    puppeteer: {
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-accelerated-2d-canvas",
+            "--no-first-run",
+            "--no-zygote",
+            "--single-process",
+            "--disable-gpu"
+        ]
+    }
 });
 
 client.on("qr", (qr) => {
