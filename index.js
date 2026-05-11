7bd458e3b4039cbe6a55befe3be751219d03de37
StarkElite <circuit369dg@hotmail.com>
2026-05-11 19:39:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const { Client, LocalAuth } = require("whatsapp-web.js");
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-    headless: true,
-    args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage"
-    ]
-}
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage"
+        ]
+    }
 });
 
 client.on("qr", (qr) => {
