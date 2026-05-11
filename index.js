2107227469b0731082f2823dabf98de8472f69e4
StarkElite <circuit369dg@hotmail.com>
2026-05-11 18:27:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const client = new Client({
 
     puppeteer: {
         headless: true,
-        executablePath: "/opt/render/.cache/puppeteer/chrome/linux-127.0.6533.88/chrome-linux64/chrome",
         args: [
             "--no-sandbox",
             "--disable-setuid-sandbox",
