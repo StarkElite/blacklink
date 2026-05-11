d8bb14bf4e2af11f966786316cbe93dfa9e59eed
StarkElite <circuit369dg@hotmail.com>
2026-05-11 18:34:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ const { Client, LocalAuth } = require("whatsapp-web.js");
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-
+    webVersionCache: {
+        type: "remote",
+        remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html"
+    },
     puppeteer: {
         headless: true,
         args: [
             "--no-sandbox",
             "--disable-setuid-sandbox",
-            "--disable-dev-shm-usage",
-            "--disable-accelerated-2d-canvas",
-            "--no-first-run",
-            "--no-zygote",
-            "--single-process",
-            "--disable-gpu"
+            "--disable-dev-shm-usage"
         ]
     }
 });
